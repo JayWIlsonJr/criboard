e230fc595982df964cf639a8c39c737a91aff488
Jay Wilson Jr <overflowacct@gmail.com>
2018-05-22 21:46:48 -0400

    setup the initial db

--- database/index.js
+++ database/index.js
@@ -1,11 +1,11 @@
 // create a database connection and export it
-
-var Sequelize = require('sequelize');
-var db = require('../config.js');
+//var user = require("./models/user");
+var Sequelize = require("sequelize");
+var db = require("../config.js");
 
 var connection = new Sequelize(db.DBNAME, db.DBUSERNAME, db.DBPASSWORD, {
   host: db.DBHOST,
-  dialect: 'mysql'
+  dialect: "mysql"
 });
 
 connection
@@ -14,7 +14,7 @@ connection
     console.log(`connection to database has been established successfully`);
   })
   .catch(err => {
-    console.log('unable to connect to database');
+    console.log("unable to connect to database");
   });
 
 // sync
@@ -22,5 +22,22 @@ connection
 connection.sync({
   logging: console.log
 });
+
+var User = connection.define("User", {
+  username: {
+    type: Sequelize.STRING
+  },
+  email: {
+    type: Sequelize.STRING
+  },
+  password: {
+    type: Sequelize.STRING
+  },
+  avatar: {
+    type: Sequelize.STRING
+  }
+});
+
+User.sync();
 
 module.exports = connection;
